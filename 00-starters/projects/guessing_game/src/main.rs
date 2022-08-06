26720146b6523580080c564f0393c5adfd0a2808
d-smith <doug.smith.mail@gmail.com>
2022-08-06 08:13:02 -0700

    added random number to guessing game, compare, shadowing

--- 00-starters/projects/guessing_game/src/main.rs
+++ 00-starters/projects/guessing_game/src/main.rs
@@ -1,6 +1,12 @@
 use std::io; //prelude
+use rand::Rng;
+use std::cmp::Ordering;
+
 
 fn main() {
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+    println!("The secret number is: {secret_number}");
+
     println!("Guess the number");
     println!("Enter your guess");
 
@@ -14,5 +20,11 @@ fn main() {
         .read_line(&mut guess) // & for reference, no copying it around...
         .expect("Failed to read line");
 
-    println!("You entered {guess}"); // {} are place holders for var vals in println
+    let guess: u32 = guess.trim().parse().expect("Please type a number!");
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too small!"),
+        Ordering::Greater => println!("Too big!"),
+        Ordering::Equal => println!("You win!"),
+    }
 }
