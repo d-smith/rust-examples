586f56f93613275f60dd16e6dc176eb2e697708b
d-smith <doug.smith.mail@gmail.com>
2022-08-07 15:12:55 -0700

    added loop, error handling on number parse example

--- 00-starters/projects/guessing_game/src/main.rs
+++ 00-starters/projects/guessing_game/src/main.rs
@@ -7,24 +7,33 @@ fn main() {
     let secret_number = rand::thread_rng().gen_range(1..=100);
     println!("The secret number is: {secret_number}");
 
-    println!("Guess the number");
-    println!("Enter your guess");
+    loop {
+        println!("Guess the number");
+        println!("Enter your guess");
 
-    let mut guess = String::new(); //new mutable string var, vars immutable by default
+        let mut guess = String::new(); //new mutable string var, vars immutable by default
 
 
-    // read_line returns a Result enum, Ok and Err can be returned
-    // if Err is returned expect will cause the program to crash and
-    // print the given error message
-    io::stdin()
-        .read_line(&mut guess) // & for reference, no copying it around...
-        .expect("Failed to read line");
+        // read_line returns a Result enum, Ok and Err can be returned
+        // if Err is returned expect will cause the program to crash and
+        // print the given error message
+        io::stdin()
+            .read_line(&mut guess) // & for reference, no copying it around...
+            .expect("Failed to read line");
 
-    let guess: u32 = guess.trim().parse().expect("Please type a number!");
+    
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
 
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too small!"),
-        Ordering::Greater => println!("Too big!"),
-        Ordering::Equal => println!("You win!"),
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
     }
 }
